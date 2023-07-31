e28a42afc774678563fd720513285dc3bfcf88f8
Joscha Konstantin Groth <joscha.groth@stud.hs-flensburg.de>
2023-07-31 17:33:01 +0200

    Add further solution for Problem2

--- Problem2/Problem2.hs
+++ Problem2/Problem2.hs
@@ -7,3 +7,15 @@ sumOfEvenFibonacciUpTo end = sumOfEvenFibonacciUpTo' 0 1 2
       | fib2 > end = sum
       | even fib2 = sumOfEvenFibonacciUpTo' (sum + fib2) fib2 (fib1 + fib2)
       | otherwise = sumOfEvenFibonacciUpTo' sum fib2 (fib1 + fib2)
+
+evenFibSum :: Int -> Int
+evenFibSum max =
+  let evenFibSum' :: Int -> Int -> Int -> Int
+      evenFibSum' fib1 fib2 akk =
+        if fib2 <= max
+          then
+            if even fib2
+              then evenFibSum' fib2 (fib1 + fib2) akk + fib2
+              else evenFibSum' fib2 (fib1 + fib2) akk
+          else akk
+   in evenFibSum' 1 2 0
